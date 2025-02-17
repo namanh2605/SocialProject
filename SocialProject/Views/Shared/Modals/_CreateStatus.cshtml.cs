084c33e726127e88fcea9dc36f7bf9ce4675d1c3
namanhnguyen2605@gmail.com <namanhnguyen2605@gmail.com>
2025-02-18 02:25:10 +0700

    Fixing index with new cshtml , createStatus , CreateStory,SuggestedFriends

---
+++ SocialProject/Views/Shared/Modals/_CreateStatus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialProject.Views.Shared.Modals
+{
+    public class _CreateStatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SocialProject/Views/Shared/Modals/_CreateStory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialProject.Views.Shared.Modals
+{
+    public class _CreateStoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
