85d6fafa2a2e4e9e244ede2897ba2764241c0b2d
namanhnguyen2605@gmail.com <namanhnguyen2605@gmail.com>
2025-02-18 23:36:15 +0700

    Adding DataContext and update new sidebar (Trend for you)

---
+++ SocialProject/Data/SocialMediaContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SocialProject.Data;
+
+public partial class SocialMediaContext : DbContext
+{
+    public SocialMediaContext()
+    {
+    }
+
+    public SocialMediaContext(DbContextOptions<SocialMediaContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var ConnectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
